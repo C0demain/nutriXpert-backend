ec51241b35cc31e23e55c01abb82ba14b50939e2
Mateus Madeira <mafemad2005@gmail.com>
2025-10-21 08:54:51 -0300

    refactor: campos string para numeros

---
+++ src/main/java/br/com/codemain/nutrixpertai/dto/User/UserPhysicalDTO.java
@@ -0,0 +1,7 @@
+package br.com.codemain.nutrixpertai.dto.User;
+
+public record UserPhysicalDTO(
+        Integer height,
+        Double weight
+) {
+}
--- src/main/java/br/com/codemain/nutrixpertai/dto/User/UserUpdateDTO.java
+++ src/main/java/br/com/codemain/nutrixpertai/dto/User/UserUpdateDTO.java
@@ -4,49 +4,17 @@ import br.com.codemain.nutrixpertai.enums.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
 
-public class UserUpdateDTO {
+public record UserUpdateDTO(
+        @Size(min = 3, max = 100, message = "Nome deve ter entre 3 e 100 caracteres")
+        String name,
 
-    @Size(min = 3, max = 100, message = "Nome deve ter entre 3 e 100 caracteres")
-    private String name;
+        @Email(message = "E-mail deve ser válido")
+        String email,
 
-    @Email(message = "E-mail deve ser válido")
-    private String email;
+        @Size(min = 6, message = "Senha deve ter pelo menos 6 caracteres")
+        String password,
 
-    @Size(min = 6, message = "Senha deve ter pelo menos 6 caracteres")
-    private String password;
-
-    private Role role;
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
+        Role role
+) {
 
 }
