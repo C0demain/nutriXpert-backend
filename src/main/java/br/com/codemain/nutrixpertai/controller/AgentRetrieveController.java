072771a4e4b9abdadc9d3eb456abfa318337b679
Mateus Madeira <mafemad2005@gmail.com>
2025-11-06 08:01:03 -0300

    feat: rotas para o agente
    rotas para o agente poder interagir com a anamnese

--- src/main/java/br/com/codemain/nutrixpertai/controller/AgentRetrieveController.java
+++ src/main/java/br/com/codemain/nutrixpertai/controller/AgentRetrieveController.java
@@ -2,48 +2,72 @@ package br.com.codemain.nutrixpertai.controller;
 
 import br.com.codemain.nutrixpertai.dto.User.UserPhysicalDTO;
 import br.com.codemain.nutrixpertai.dto.User.UserResponseDTO;
+import br.com.codemain.nutrixpertai.dto.anamnese.AnamneseRequestDTO;
+import br.com.codemain.nutrixpertai.dto.anamnese.AnamneseResponseDTO;
+import br.com.codemain.nutrixpertai.service.IAnamneseService;
 import br.com.codemain.nutrixpertai.service.IUserService;
 import br.com.codemain.nutrixpertai.service.impl.AgentServiceImpl;
+import br.com.codemain.nutrixpertai.service.impl.AnamneseServiceImpl;
 import br.com.codemain.nutrixpertai.service.impl.UserServiceImpl;
 import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.UUID;
 
 @Tag(name = "Agente IA", description = "Endpoints para o agente de IA interagir com o backend")
 @RestController
 @CrossOrigin
 @RequestMapping("/api/interact/agent")
-@Hidden
+//@Hidden
 @SecurityRequirement(name = "Bearer Authentication")
 public class AgentRetrieveController {
 
     private final AgentServiceImpl agentService;
     private final IUserService userService;
+    private final IAnamneseService anamneseService;
 
-    public AgentRetrieveController(AgentServiceImpl agentServiceImpl, UserServiceImpl userService) {
+
+    public AgentRetrieveController(AgentServiceImpl agentServiceImpl, UserServiceImpl userService, AnamneseServiceImpl anamneseService) {
         this.agentService = agentServiceImpl;
         this.userService = userService;
+        this.anamneseService = anamneseService;
     }
 
     @GetMapping(value = "/getUserInfo/{id}")
-    @Operation(summary = "Busca usuário por ID")
     public ResponseEntity<UserResponseDTO> getById(@PathVariable("id") UUID id) {
         UserResponseDTO user = userService.getById(id);
         return ResponseEntity.ok().body(user);
     }
 
     @PatchMapping(value = "physical/{id}")
-    @Operation(summary = "Atualiza peso e altura do usuário")
     public ResponseEntity<UserResponseDTO> updateUserWeight(
             @PathVariable("id") UUID id,
             @RequestBody UserPhysicalDTO userPhysicalDTO) {
         UserResponseDTO updated = userService.updatePhysical(id, userPhysicalDTO);
 
         return ResponseEntity.ok().body(updated);
+    }
+
+    @PostMapping("/{userId}/anamnese")
+    public ResponseEntity<UserResponseDTO> create(
+            @PathVariable UUID userId,
+            @RequestBody AnamneseRequestDTO requestDTO) {
+        UserResponseDTO responseDTO = anamneseService.createAgent(userId, requestDTO);
+        return ResponseEntity.ok().body(responseDTO);
+    }
+
+    @PatchMapping("/{userId}/anamnese")
+    public ResponseEntity<UserResponseDTO> patch(
+            @PathVariable UUID userId,
+            @RequestBody AnamneseRequestDTO patchRequest) {
+        UserResponseDTO responseDTO = anamneseService.patchAgent(userId, patchRequest);
+        return ResponseEntity.ok(responseDTO);
     }
 }
