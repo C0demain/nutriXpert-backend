b060b59b0dc81121f525d66c690af9a9612f793c
Abner Douglas <abner.rmachado@gmail.com>
2025-11-08 19:44:51 -0300

    fix: corrige mapeamento do id do usuario ao retornar os obejtivos

--- src/main/java/br/com/codemain/nutrixpertai/service/mapper/GoalMapper.java
+++ src/main/java/br/com/codemain/nutrixpertai/service/mapper/GoalMapper.java
@@ -4,13 +4,16 @@ import br.com.codemain.nutrixpertai.dto.Goal.CreateGoalDTO;
 import br.com.codemain.nutrixpertai.dto.Goal.GoalResponseDTO;
 import br.com.codemain.nutrixpertai.entity.Goal;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface GoalMapper {
+    @Mapping(target = "userId", source = "user.id")
     GoalResponseDTO toResponseDTO(Goal goal);
 
+    @Mapping(target = "user", ignore = true)
     Goal toEntity(CreateGoalDTO dto);
 
     List<GoalResponseDTO> toResponseDTOList(List<Goal> goals);
